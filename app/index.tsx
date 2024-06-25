d2e0ceb4e6e5d2dbaf41e9c1aed38d6b97af6651
hzz780 <hzz780@qq.com>
2024-06-25 18:49:51 +0800

    ci: 🎡 update logo; add webview

--- app/index.tsx
+++ app/index.tsx
@@ -1,40 +1,18 @@
-import { Text, View } from "react-native";
-// import { WebView } from 'react-native-webview';
-import Constants from 'expo-constants';
+import { WebView } from 'react-native-webview';
 import { StyleSheet } from 'react-native';
 
 export default function Index() {
-  // return (
-  //   <WebView
-  //     style={styles.container}
-  //     source={{ uri: 'https://docs.aelf.io' }}
-  //   />
-  // );
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.!!!</Text>
-      {/*<WebView*/}
-      {/*  style={styles.container}*/}
-      {/*  originWhitelist={['*']}*/}
-      {/*  source={{ html: '<h1><center>Hello world</center></h1>' }}*/}
-      {/*/>*/}
-      {/*<WebView*/}
-      {/*  style={styles.container}*/}
-      {/*  source={{ uri: 'https://docs.aelf.io' }}*/}
-      {/*/>*/}
-    </View>
+    <WebView
+      style={styles.container}
+      source={{ uri: 'https://docs.aelf.io' }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Constants.statusBarHeight,
+    marginTop: 0,
   },
 });
