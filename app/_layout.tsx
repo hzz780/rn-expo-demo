b5130c1e8054a49ac6c474bbcf26384d220ea617
hzz780 <hzz780@qq.com>
2024-06-25 19:05:58 +0800

    feat: 🎸 change title

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,9 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="Learn programming" />
+      <Stack.Screen name="index" options={{
+        title: 'Learn Program',
+      }} />
     </Stack>
   );
 }
